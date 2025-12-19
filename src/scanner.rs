eca774f1dc5eb81c6747e05dac3b1147d88989ef
zooeywm <zooeywm@gmail.com>
2025-12-19 14:27:22 +0800

    feat: Support `keyword` and `identifier` and utf8 `string`

--- src/scanner.rs
+++ src/scanner.rs
@@ -18,27 +18,36 @@
 //!
 //! The rules that determine how a particular language groups characters into
 //! lexemes are called its `lexical grammar`.
+//!
+//! We can’t easily detect a `reserved word` until we’ve reached the end of what
+//! might instead be an identifier, this is `maximal munch`.
+
+use std::collections::HashMap;
 
 use TokenType::*;
 use anyhow::Context;
 
 use crate::error::ScanErrors;
 
+/// A scanner for Lox source code
 #[derive(Default)]
 pub struct Scanner<'a> {
 	/// User input source code
-	source:  &'a str,
+	source:   &'a str,
 	/// Lexed tokens
-	tokens:  Vec<Token<'a>>,
+	tokens:   Vec<Token<'a>>,
 	/// Points to the first character in the lexeme being scanned
-	start:   usize,
+	start:    usize,
 	/// Points at the character currently being considered
-	current: usize,
+	current:  usize,
 	/// Tracks what source line `current` is on so we can produce tokens that know
 	/// their location.
-	line:    usize,
+	line:     usize,
+	/// Reserved keywords in Lox
+	keywords: HashMap<&'a str, TokenType<'a>>,
 }
 
+/// A token produced by the scanner
 #[derive(Debug)]
 pub struct Token<'a> {
 	r#type: TokenType<'a>,
@@ -47,14 +56,37 @@ pub struct Token<'a> {
 }
 
 impl<'a> Scanner<'a> {
-	pub fn new(source: &'a str) -> Self { Self { source, ..Default::default() } }
+	pub fn new(source: &'a str) -> Self {
+		let keywords = HashMap::from([
+			("and", And),
+			("class", Class),
+			("else", Else),
+			("false", False),
+			("for", For),
+			("fun", Fun),
+			("if", If),
+			("nil", Nil),
+			("or", Or),
+			("print", Print),
+			("return", Return),
+			("super", Super),
+			("this", This),
+			("true", True),
+			("var", Var),
+			("while", While),
+		]);
 
+		Self { source, keywords, ..Default::default() }
+	}
+
+	/// Scan all tokens from the source code
 	pub fn scan_tokens(&'a mut self) -> crate::Result<&'a [Token<'a>]> {
 		let mut scan_errors = ScanErrors::new();
 		while !self.is_at_end() {
 			// We are at the beginning of the next lexeme.
 			self.start = self.current;
 			if let Err(e) = self.scan_token() {
+				eprintln!("{e}");
 				scan_errors.push(self.line, e.to_string());
 			}
 		}
@@ -65,8 +97,7 @@ impl<'a> Scanner<'a> {
 		Ok(self.tokens.as_slice())
 	}
 
-	fn is_at_end(&self) -> bool { self.current >= self.source.len() }
-
+	/// Scan a single token from the source code
 	fn scan_token(&mut self) -> crate::Result<()> {
 		let next_char = self.advance()?;
 		#[rustfmt::skip]
@@ -88,50 +119,56 @@ impl<'a> Scanner<'a> {
             '/' => if self.match_next('/')? {
                         while self.peek()? != '\n' && !self.is_at_end() { self.advance()?; }
                         Comment
-                    } else { Slash },
-            ' ' => EmptyChar,
-            '\r'=> EmptyChar,
-            '\t'=> EmptyChar,
+                   } else { Slash },
+            ' ' | '\r' | '\t' => EmptyChar,
             '\n'=> { self.line += 1; NewLine }
-            '"' => StringLiteral(self.string()?),
-			_ => {
-				return Err(format!("Unsupported character '{next_char}' for TokenType conversion").into());
-			}
+            '"' => self.string()?,
+            char if char.is_ascii_digit() => self.number()?,
+            char if char.is_ascii_alphabetic() || char == '_' => self.identifier()?,
+			_ => return Err(format!("Unsupported character '{next_char}' for TokenType conversion").into())
 		};
-		let lexeme =
-			self.source.get(self.start..self.current).context("slice indices must be on char boundaries")?;
+		let lexeme = self.get_slice(self.start, self.current)?;
 		let line = self.line;
 		if !r#type.is_ignored() {
 			self.tokens.push(Token { r#type, lexeme, line });
 		}
 		Ok(())
 	}
+}
 
+impl<'a> Scanner<'a> {
+	/// Check if we have reached the end of the source code
+	fn is_at_end(&self) -> bool { self.current >= self.source.chars().count() }
+
+	/// Match the next character if it is the expected one
 	fn match_next(&mut self, expected: char) -> crate::Result<bool> {
 		if self.is_at_end() {
 			return Ok(false);
 		}
-		if self.source.chars().nth(self.current).context("Scan out of bound")? != expected {
+		if self.source.chars().nth(self.current).context("Scan out of bound: match_next")? != expected {
 			return Ok(false);
 		}
 		self.current += 1;
 		Ok(true)
 	}
 
+	/// Advance to the next character
 	fn advance(&mut self) -> crate::Result<char> {
-		let ch = self.source.chars().nth(self.current).context("Scan out of bound")?;
+		let ch = self.source.chars().nth(self.current).context("Scan out of bound: advance")?;
 		self.current += 1;
 		Ok(ch)
 	}
 
+	/// Peek the current character
 	fn peek(&self) -> crate::Result<char> {
 		if self.is_at_end() {
 			return Ok('\0');
 		}
-		Ok(self.source.chars().nth(self.current).context("Scan out of bound")?)
+		Ok(self.source.chars().nth(self.current).context("Scan out of bound: peek")?)
 	}
 
-	fn string(&mut self) -> crate::Result<&'a str> {
+	/// Scan a string literal
+	fn string(&mut self) -> crate::Result<TokenType<'a>> {
 		while self.peek()? != '"' && !self.is_at_end() {
 			if self.peek()? == '\n' {
 				self.line += 1
@@ -145,21 +182,81 @@ impl<'a> Scanner<'a> {
 
 		self.advance()?; // The closing "
 
-		let ret = self
-			.source
-			.get(self.start + 1..self.current - 1)
-			.context("Slice indices must be on char boundaries")?;
-		Ok(ret)
+		let ret = self.get_slice(self.start + 1, self.current - 1)?;
+		Ok(TokenType::StringLiteral(ret))
+	}
+
+	/// Scan a number literal
+	fn number(&mut self) -> crate::Result<TokenType<'a>> {
+		while self.peek()?.is_ascii_digit() {
+			self.advance()?;
+		}
+
+		// Look for a fractional part.
+		if self.peek()? == '.' && self.peek_second()?.is_ascii_digit() {
+			// Consume the "."
+			self.advance()?;
+
+			while self.peek()?.is_ascii_digit() {
+				self.advance()?;
+			}
+		}
+
+		let number_str = self.get_slice(self.start, self.current)?;
+		let number_value: f64 = number_str.parse().context("Failed to parse number literal")?;
+		Ok(TokenType::NumberLiteral(number_value))
+	}
+
+	/// Peek the second character
+	fn peek_second(&self) -> crate::Result<char> {
+		if self.current + 1 >= self.source.chars().count() {
+			return Ok('\0');
+		}
+		Ok(self.source.chars().nth(self.current + 1).context("Scan out of bound: peek_second")?)
+	}
+
+	fn identifier(&mut self) -> crate::Result<TokenType<'a>> {
+		while self.peek()?.is_ascii_alphanumeric() || self.peek()? == '_' {
+			self.advance()?;
+		}
+		let text = self.get_slice(self.start, self.current)?;
+		match self.keywords.get(text) {
+			Some(keyword_type) => Ok(keyword_type.clone()),
+			None => Ok(TokenType::Identifier(text)),
+		}
+	}
+
+	fn get_slice(&self, start: usize, end: usize) -> crate::Result<&'a str> {
+		if start > end {
+			return Err(anyhow::anyhow!("Invalid char range [{start}, {end})").into());
+		}
+
+		let mut iter = self.source.char_indices();
+
+		let start_byte =
+			iter.nth(start).map(|(i, _)| i).context(format!("Start index {start} out of char bounds"))?;
+
+		let end_byte = if end == start {
+			start_byte
+		} else {
+			iter.nth(end - start - 1).map(|(i, c)| i + c.len_utf8()).unwrap_or(self.source.len())
+		};
+
+		Ok(self.source.get(start_byte..end_byte).context(format!(
+			"Char slice [{start}, {end}) maps to invalid byte range [{start_byte}, {end_byte})"
+		))?)
 	}
 }
 
-#[derive(Debug)]
+/// The different types of tokens in Lox, The copying is lightweight
+#[derive(Debug, Clone)]
 pub enum TokenType<'a> {
+	Undefined,
 	/// New Line Character `\n`.
 	NewLine,
 	/// Empty Character: ` `, `\r`, `\t`.
 	EmptyChar,
-	/// Comment `//`
+	/// Comment `//` or /* ... */
 	Comment,
 	/// Left parenthesis `(`.
 	LeftParen,
